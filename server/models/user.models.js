ea4a8baaf046cdf9e84bcb13b60fda9ef4bc314e
arel hagag <areltv297@gmail.com>
2022-12-27 12:43:38 +0200

    .

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs"
+
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -7,26 +9,44 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
-  email: { type: String,required: true,
+  email: {
+    type: String,
+    required: true,
     trim: true,
     lowercase: true,
     validate(value) {
-        if (!validator.isEmail(value)) {
-            throw new Error('Email is invalid')
-        }
-    } },
-    password: { type: String,required: true,
-      minlength: 8,
-      trim: true,
-      validate(value) {
-          if (value.toLowerCase().includes('password')) {
-              throw new Error('Password cannot contain "password"')
-          }
-      } },
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
+  address: { type: String, required: true, trim: true, lowercase: true },
   phoneNumber: { type: Number },
-  address: { type: String },
 });
 
 const ShopUsers = mongoose.model("ShopUsers", userSchema);
 
 export { ShopUsers };
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+export const User = mongoose.model("User", userSchema);
